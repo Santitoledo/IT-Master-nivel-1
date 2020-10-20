6fd9d3b004c5c8d8f38e9c477a75b3d34d71b51c
Santiago Toledo <69609586+Santitoledo@users.noreply.github.com>
2020-10-20 09:59:50 -0300

    Create header1.js

---
+++ Clase 20/ejercicio 2/src/header1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const Header = () => (
+    <header id='header'>
+    <img src="https://png.pngtree.com/element_our/sm/20180415/sm_5ad31d9b53530.jpg" alt="logo"/>
+    <div id='titulo'>
+    <h1>Música</h1> 
+    </div>
+    </header>
+) 
+
+export default Header;
